5ec255f3d3b7c1345648b375f27b941be2ca0eeb
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-28 02:32:08 +0300

    feat: create user request dto

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/request/RegisterUserRequest.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/request/RegisterUserRequest.java
@@ -1,4 +1,33 @@
 package com.sustainabilitytracker.sustainabilitytracker.dtos.request;
 
+import com.sustainabilitytracker.sustainabilitytracker.enums.Role;
+import com.sustainabilitytracker.sustainabilitytracker.validation.Lowercase;
+import com.sustainabilitytracker.sustainabilitytracker.validation.ValidRegisterUser;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@ValidRegisterUser
+@Data
 public class RegisterUserRequest {
+
+    @NotBlank(message = "Full name is required.")
+    private String fullName;
+
+    @Email
+    @NotBlank(message = "Email is required.")
+    @Lowercase(message = "Email must be lowercase.")
+    private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min = 8, message = "Password must be at least 8 characters.")
+    private String password;
+
+    @NotNull
+    private Role role;
+
+    private Long companyId;
+    private Long departmentId;
 }
