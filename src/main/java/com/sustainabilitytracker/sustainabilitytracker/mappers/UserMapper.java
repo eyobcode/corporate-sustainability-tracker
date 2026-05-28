be5219d503e64a7717a503ff79ef17ecfa4d924c
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-28 15:23:42 +0300

    feat: we created register rout with full logic

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/UserMapper.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/UserMapper.java
@@ -6,15 +6,17 @@ import com.sustainabilitytracker.sustainabilitytracker.dtos.response.UserRespons
 import com.sustainabilitytracker.sustainabilitytracker.entities.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
     UserDto toDto(User user);
+
+    @Mapping(target = "company", ignore = true)
+    @Mapping(target = "department", ignore = true)
+    User toEntity(RegisterUserRequest request);
 
     @Mapping(source = "company.name", target = "companyName")
     @Mapping(source = "department.name", target = "departmentName")
     UserResponse toResponse(User user);
-
-    User toEntity(RegisterUserRequest user);
-//    void update(UpdateUserRequest request, @MappingTarget User user);
 }
