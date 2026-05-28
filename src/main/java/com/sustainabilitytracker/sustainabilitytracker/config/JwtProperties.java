1769591ec3720f05ead2ff244b4253ea0e6245aa
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-28 16:49:58 +0300

    feat: create jwt provider

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/config/JwtProperties.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/config/JwtProperties.java
@@ -1,8 +1,11 @@
 package com.sustainabilitytracker.sustainabilitytracker.config;
 
+import io.jsonwebtoken.security.Keys;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
 
 @Data
 @Component
@@ -10,4 +13,10 @@ import org.springframework.stereotype.Component;
 public class JwtProperties {
     private String secret;
     private long expiration;
+    private long accessTokenExpiration;
+    private long refreshTokenExpiration;
+
+    public SecretKey getSecretKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
 }
