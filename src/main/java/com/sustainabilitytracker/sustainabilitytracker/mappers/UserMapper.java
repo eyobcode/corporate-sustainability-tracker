f2c6ff41e1414e0f64c44543eba32dae0f5bcc9a
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-28 03:01:20 +0300

    refactor: user mapper with relevant name

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/UserMapper.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/UserMapper.java
@@ -1,14 +1,19 @@
 package com.sustainabilitytracker.sustainabilitytracker.mappers;
 
+import com.sustainabilitytracker.sustainabilitytracker.dtos.UserDto;
 import com.sustainabilitytracker.sustainabilitytracker.dtos.request.RegisterUserRequest;
 import com.sustainabilitytracker.sustainabilitytracker.dtos.response.UserResponse;
 import com.sustainabilitytracker.sustainabilitytracker.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-//    UserDto toDto(User user);
-    UserResponse toDto(User user);
+    UserDto toDto(User user);
+
+    @Mapping(source = "company.name", target = "companyName")
+    @Mapping(source = "department.name", target = "departmentName")
+    UserResponse toResponse(User user);
 
     User toEntity(RegisterUserRequest user);
 //    void update(UpdateUserRequest request, @MappingTarget User user);
