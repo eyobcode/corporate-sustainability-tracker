136427d0f9e73d79d5bacf155a692a01e4a3d581
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-09 13:57:03 +0300

    feat: Add energy service

---
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/services/EnergyService.java
@@ -0,0 +1,10 @@
+package com.sustainabilitytracker.sustainabilitytracker.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EnergyService {
+
+}
