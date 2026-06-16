0b27f2a0cef36e876e5df84a4672784b7ee226c5
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-17 01:36:45 +0300

    feat: add waste controller.

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/controllers/WasteController.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/controllers/WasteController.java
@@ -1,4 +1,24 @@
 package com.sustainabilitytracker.sustainabilitytracker.controllers;
 
+import com.sustainabilitytracker.sustainabilitytracker.dtos.request.RejectRequest;
+import com.sustainabilitytracker.sustainabilitytracker.dtos.request.WasteRequest;
+import com.sustainabilitytracker.sustainabilitytracker.dtos.response.WasteResponse;
+import com.sustainabilitytracker.sustainabilitytracker.dtos.response.WasteSummaryResponse;
+import com.sustainabilitytracker.sustainabilitytracker.repositories.WasteRepository;
+import com.sustainabilitytracker.sustainabilitytracker.services.WasteService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/waste")
 public class WasteController {
 }
