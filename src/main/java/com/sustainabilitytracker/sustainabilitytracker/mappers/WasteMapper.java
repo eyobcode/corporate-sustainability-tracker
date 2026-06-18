f866216451126de27c3ed5aafbdc8c5e68f4837c
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-18 12:06:30 +0300

    fix(mappers): add , unmappedTargetPolicy = ReportingPolicy.IGNORE to all mapper to avoid ignored fields

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/WasteMapper.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/WasteMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.*;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface WasteMapper {
 
     @Mapping(target = "company", ignore = true)
