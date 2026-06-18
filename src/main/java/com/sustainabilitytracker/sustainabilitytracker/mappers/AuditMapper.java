15455c62f99428fc3ee5e4e8f28982d1aaa81b97
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-18 12:06:43 +0300

    fix(mappers): add , unmappedTargetPolicy = ReportingPolicy.IGNORE to all mapper to avoid ignored fields

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/AuditMapper.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/AuditMapper.java
@@ -4,10 +4,11 @@ import com.sustainabilitytracker.sustainabilitytracker.dtos.response.AuditRespon
 import com.sustainabilitytracker.sustainabilitytracker.entities.AuditRecord;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AuditMapper {
 
     @Mapping(target = "reportId", source = "report.id")
