c9652199254de3a2e511002523c79f8cef370c77
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-21 02:58:39 +0300

    refactor: change few line of code and improve few fields

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/repositories/CompanyRepository.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/repositories/CompanyRepository.java
@@ -4,23 +4,21 @@ import com.sustainabilitytracker.sustainabilitytracker.entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
     boolean existsByName(String name);
-    List<Company> findByIsActiveTrue();
+
     boolean existsByNameAndIdNot(String name, Long id);
-    List<Company> findAllByIsActive(Boolean isActive);
 
     Optional<Company> findByIdAndIsActiveTrue(Long companyId);
-    boolean existsByIdAndIsActiveTrue(Long companyId);
-
-    @Query("SELECT c.co2Target FROM Company c WHERE c.id = :id")
-    BigDecimal getCo2Target(@Param("id") Long id);
 
     Long countByIsActiveTrue();
 
