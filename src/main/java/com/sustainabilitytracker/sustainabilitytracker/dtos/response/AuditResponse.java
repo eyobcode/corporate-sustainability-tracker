d919fecc4225c0c87e467adce6b12a80572d06fd
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-22 01:26:33 +0300

    refactor: change few line of code and improve few fields

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/AuditResponse.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/AuditResponse.java
@@ -1,15 +1,18 @@
 package com.sustainabilitytracker.sustainabilitytracker.dtos.response;
 
 import com.sustainabilitytracker.sustainabilitytracker.enums.AuditAction;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuditResponse {
-
     private Long id;
     private Long reportId;
     private String reportTitle;
@@ -18,5 +21,5 @@ public class AuditResponse {
     private AuditAction action;
     private String comments;
     private String flaggedItems;
-    private Instant reviewedAt;
+    private Instant createdAt;
 }
