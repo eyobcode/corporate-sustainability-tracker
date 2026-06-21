0363c2c496fa675a836389c5faa586bb912dac3f
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-22 01:26:41 +0300

    refactor: change few line of code and improve few fields

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/DepartmentResponse.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/DepartmentResponse.java
@@ -1,17 +1,25 @@
 package com.sustainabilitytracker.sustainabilitytracker.dtos.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DepartmentResponse {
     private Long id;
     private String name;
     private String description;
     private String companyName;
     private Boolean isActive;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd MMM yyyy, HH:mm", timezone = "UTC")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+            pattern = "dd MMM yyyy, HH:mm",
+            timezone = "UTC")
     private Instant createdAt;
 }
