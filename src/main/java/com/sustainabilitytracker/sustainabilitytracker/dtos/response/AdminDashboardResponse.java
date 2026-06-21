6f2d4091224b4deb93c9db4df63d758ddf4d86da
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-22 01:26:30 +0300

    refactor: change few line of code and improve few fields

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/AdminDashboardResponse.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/AdminDashboardResponse.java
@@ -1,6 +1,5 @@
 package com.sustainabilitytracker.sustainabilitytracker.dtos.response;
 
-import com.sustainabilitytracker.sustainabilitytracker.entities.SustainabilityScore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
