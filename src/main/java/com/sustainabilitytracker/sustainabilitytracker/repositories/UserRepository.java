5914f12d3f84c4f08b55e8983d545c230703046c
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-21 03:11:17 +0300

    refactor: change few line of code and improve few fields

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/repositories/UserRepository.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.sustainabilitytracker.sustainabilitytracker.repositories;
 
 import com.sustainabilitytracker.sustainabilitytracker.entities.User;
+import com.sustainabilitytracker.sustainabilitytracker.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,5 +15,15 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
+    boolean existsByEmailAndIdNot(String email, Long id);
+
     Long countByIsActiveTrue();
+
+    List<User> findByCompanyId(Long companyId);
+
+    List<User> findByDepartmentId(Long departmentId);
+
+    List<User> findByCompanyIdAndRole(Long companyId, Role role);
+
+    List<User> findByCompanyIdAndIsActiveTrue(Long companyId);
 }
