858314c521081dd12aaacd206d8f7573d6992c28
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-22 03:26:54 +0300

    refactor(auth): improve authentication service and refresh logic
    - add new login response
    - Extracted helper methods for better readability and reusability
    - Added refresh token rotation for improved security
    - Enhanced error handling and validation
    - Simplified login and refresh flows
    - Cleaned up cookie handling and response building

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/SustainabilityTrackerApplication.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/SustainabilityTrackerApplication.java
@@ -1,7 +1,13 @@
 package com.sustainabilitytracker.sustainabilitytracker;
 
+import com.sustainabilitytracker.sustainabilitytracker.entities.User;
+import com.sustainabilitytracker.sustainabilitytracker.enums.Role;
+import com.sustainabilitytracker.sustainabilitytracker.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SustainabilityTrackerApplication {
@@ -9,5 +15,23 @@ public class SustainabilityTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SustainabilityTrackerApplication.class, args);
     }
+    @Bean
+    CommandLineRunner createDefaultUser(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            String email = "eyob@dev.com";
 
+            if (userRepository.findByEmail(email).isEmpty()) {
+                User admin = new User();
+                admin.setEmail(email);
+                admin.setFullName("Eyob Dev");
+                admin.setPassword(passwordEncoder.encode("password123"));
+                admin.setRole(Role.ADMIN);
+                admin.setIsActive(true);
+                admin.setIsFirstLogin(true);
+
+                userRepository.save(admin);
+                System.out.println("✅ Default admin user created: " + email + " / password123");
+            }
+        };
+    }
 }
